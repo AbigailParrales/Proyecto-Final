63edca9775617f1b75d9d978e9c9c39cb1dcbe70
AbigailParrales <danielaparralesd.m@gmail.com>
2017-04-21 14:01:16 -0500

    a-constructor Ventana

--- Ventana.java
+++ Ventana.java
@@ -1,6 +1,9 @@
+import javax.swing.JFrame;
 
-public class Ventana {
-
+public class Ventana extends JFrame{
+	public Ventana(){
+		super("Paint");		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
