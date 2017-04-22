4a74d5332707bc8d43ce2c3e38d29e6d295b1faf
AbigailParrales <danielaparralesd.m@gmail.com>
2017-04-22 17:06:08 -0500

    submitted

--- Figura.java
+++ Figura.java
@@ -1,4 +1,12 @@
 
 public class Figura {
-
+	
+	private double wide,
+				   height,
+				   x,
+				   y;
+	
+	public Figura(){
+		//this.wide
+	}
 }
--- PanelControles.java
+++ PanelControles.java
@@ -1,28 +1,28 @@
 import java.awt.Color;
 import java.awt.Dimension;
 
-import javax.swing.JButton;
+import javax.swing.Icon;
 import javax.swing.JPanel;
 
 public class PanelControles extends JPanel{
 
-	private JButton btUpload,
-					btPencil,
-					btColor,
-					btSquare,
-					btCircle,
-					btTriangle,
-					btLine,
-					btSeal1,
-					btSeal2,
-					btSeal3,
-					btSeal4,
-					btSeal5;
+	private Icon icUpload,
+				 icPencil,
+				 icColor,
+				 icSquare,
+				 icCircle,
+				 icTriangle,
+				 icLine,
+				 icSeal1,
+				 icSeal2,
+				 icSeal3,
+				 icSeal4,
+				 icSeal5;
 	
 	public PanelControles(){
 		super();
 		this.setPreferredSize(new Dimension(200,700));
-		this.setBackground(Color.CYAN);
+		this.setBackground(new Color(204,153,255));
 		
 		
 	}
