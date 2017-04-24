0e088bd19a4193101f40cf575977c8c930de9b8a
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2017-04-24 17:39:51 -0500

    Add files via upload

--- PanelControles.java
+++ PanelControles.java
@@ -1,15 +1,46 @@
-
+/*
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
+
+import javax.swing.Icon;
+import javax.swing.JPanel;
+public class PanelControles extends JPanel{
+
+	private Icon icUpload,
+				 icPencil,
+				 icColor,
+				 icSquare,
+				 icCircle,
+				 icTriangle,
+				 icLine,
+				 icSeal1,
+				 icSeal2,
+				 icSeal3,
+				 icSeal4,
+				 icSeal5;
+	
+	public PanelControles(){
+		super();
+		this.setPreferredSize(new Dimension(200,700));
+		this.setBackground(new Color(204,153,255));
+		
+		
+	}
+}
+*/
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
-public class PanelControles extends JPanel{
-	private JButton fotoB,
+public class PanelControles extends JPanel implements ActionListener{
+	private JRadioButton fotoB,
 					pincelB,
 					borradorB,
 					paletaB,
@@ -28,61 +59,71 @@ public class PanelControles extends JPanel{
 						sello3,
 						sello4,
 						guardar;
+
 	
 	public PanelControles(){
 		super();
 		this.setPreferredSize(new Dimension(200,700));
-		this.setBackground(new Color(0,200,255));
+		this.setBackground(new Color(204,153,255));
 		
 		this.paintL=new JLabel("Paint Remasterizado");
+		//this.paintL
 		this.add(paintL);
-		paintL.setFont(new Font("Helvetica", Font.BOLD, 20));
 		
 		this.foto=new ImageIcon("foto.png");
-		this.fotoB=new JButton(foto);
+		this.fotoB=new JRadioButton(foto);
+		this.fotoB.setPreferredSize(new Dimension(80,80));
 		this.add(this.fotoB);
-		this.fotoB.setPreferredSize(new Dimension(90,90));
 		
 		this.pincel=new ImageIcon("pincel.png");
-		this.pincelB=new JButton(pincel);
+		this.pincelB=new JRadioButton(pincel);
+		this.pincelB.setPreferredSize(new Dimension(80,80));
 		this.add(pincelB);
-		this.pincelB.setPreferredSize(new Dimension(90,90));
 		
 		this.borrador=new ImageIcon("borrador.png");
-		this.borradorB=new JButton(borrador);
+		this.borradorB=new JRadioButton(borrador);
+		this.borradorB.setPreferredSize(new Dimension(80,80));
 		this.add(borradorB);
-		this.borradorB.setPreferredSize(new Dimension(90,90));
 		
 		this.paleta=new ImageIcon("paleta.png");
-		this.paletaB=new JButton(paleta);
+		this.paletaB=new JRadioButton(paleta);
+		this.paletaB.setPreferredSize(new Dimension(80,80));
 		this.add(paletaB);
-		this.paletaB.setPreferredSize(new Dimension(90,90));
 		
 		this.sello1=new ImageIcon("sello1.png");
-		this.sello1B=new JButton(sello1);
+		this.sello1B=new JRadioButton(sello1);
+		this.sello1B.setPreferredSize(new Dimension(80,80));
 		this.add(sello1B);
-		this.sello1B.setPreferredSize(new Dimension(90,90));
 		
 		this.sello2=new ImageIcon("sello2.png");
-		this.sello2B=new JButton(sello2);
+		this.sello2B=new JRadioButton(sello2);
+		this.sello2B.setPreferredSize(new Dimension(80,80));
 		this.add(sello2B);
-		this.sello2B.setPreferredSize(new Dimension(90,90));
 		
 		this.sello3=new ImageIcon("sello3.png");
-		this.sello3B=new JButton(sello3);
+		this.sello3B=new JRadioButton(sello3);
+		this.sello3B.setPreferredSize(new Dimension(80,80));
 		this.add(sello3B);
-		this.sello3B.setPreferredSize(new Dimension(90,90));
 		
 		this.sello4=new ImageIcon("sello4.png");
-		this.sello4B=new JButton(sello4);
+		this.sello4B=new JRadioButton(sello4);
+		this.sello4B.setPreferredSize(new Dimension(80,80));
 		this.add(sello4B);
-		this.sello4B.setPreferredSize(new Dimension(90,90));
 		
 		this.guardar=new ImageIcon("guardar.png");
-		this.guardarB=new JButton(guardar);
+		this.guardarB=new JRadioButton(guardar);
+		this.guardarB.setPreferredSize(new Dimension(80,80));
+		this.guardarB.addActionListener(this);
 		this.add(guardarB);
-		this.guardarB.setPreferredSize(new Dimension(90,90));
+		
+		
 		
 	}
-}
 
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==this.guardarB){
+			System.out.println("Guardar");
+		}
+	}
+}
