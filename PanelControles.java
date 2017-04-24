bb0df02ab2eda62af29a19f4b6c137c3ee015015
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2017-04-24 17:49:01 -0500

    JRadioButtons

--- PanelControles.java
+++ PanelControles.java
@@ -30,15 +30,17 @@ public class PanelControles extends JPanel{
 */
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
-public class PanelControles extends JPanel{
-	private JButton fotoB,
+public class PanelControles extends JPanel implements ActionListener{
+	private JRadioButton fotoB,
 					pincelB,
 					borradorB,
 					paletaB,
@@ -57,61 +59,71 @@ public class PanelControles extends JPanel{
 						sello3,
 						sello4,
 						guardar;
+
 	
 	public PanelControles(){
 		super();
 		this.setPreferredSize(new Dimension(200,700));
-		this.setBackground(new Color(204,255,102));
+		this.setBackground(new Color(204,153,255));
 		
-		this.paintL=new JLabel("Paint");
-		this.paintL.setFont(new Font("Calibri",Font.BOLD,60));
-		this.paintL.setForeground(Color.WHITE);
+		this.paintL=new JLabel("Paint Remasterizado");
+		//this.paintL
 		this.add(paintL);
 		
 		this.foto=new ImageIcon("foto.png");
-		this.fotoB=new JButton(foto);
+		this.fotoB=new JRadioButton(foto);
+		this.fotoB.setPreferredSize(new Dimension(80,80));
 		this.add(this.fotoB);
-		this.fotoB.setPreferredSize(new Dimension(64,64));
 		
 		this.pincel=new ImageIcon("pincel.png");
-		this.pincelB=new JButton(pincel);
+		this.pincelB=new JRadioButton(pincel);
+		this.pincelB.setPreferredSize(new Dimension(80,80));
 		this.add(pincelB);
-		this.pincelB.setPreferredSize(new Dimension(64,64));
 		
 		this.borrador=new ImageIcon("borrador.png");
-		this.borradorB=new JButton(borrador);
+		this.borradorB=new JRadioButton(borrador);
+		this.borradorB.setPreferredSize(new Dimension(80,80));
 		this.add(borradorB);
-		this.borradorB.setPreferredSize(new Dimension(64,64));
 		
 		this.paleta=new ImageIcon("paleta.png");
-		this.paletaB=new JButton(paleta);
+		this.paletaB=new JRadioButton(paleta);
+		this.paletaB.setPreferredSize(new Dimension(80,80));
 		this.add(paletaB);
-		this.paletaB.setPreferredSize(new Dimension(64,64));
 		
 		this.sello1=new ImageIcon("sello1.png");
-		this.sello1B=new JButton(sello1);
+		this.sello1B=new JRadioButton(sello1);
+		this.sello1B.setPreferredSize(new Dimension(80,80));
 		this.add(sello1B);
-		this.sello1B.setPreferredSize(new Dimension(64,64));
 		
 		this.sello2=new ImageIcon("sello2.png");
-		this.sello2B=new JButton(sello2);
+		this.sello2B=new JRadioButton(sello2);
+		this.sello2B.setPreferredSize(new Dimension(80,80));
 		this.add(sello2B);
-		this.sello2B.setPreferredSize(new Dimension(64,64));
 		
 		this.sello3=new ImageIcon("sello3.png");
-		this.sello3B=new JButton(sello3);
+		this.sello3B=new JRadioButton(sello3);
+		this.sello3B.setPreferredSize(new Dimension(80,80));
 		this.add(sello3B);
-		this.sello3B.setPreferredSize(new Dimension(64,64));
 		
 		this.sello4=new ImageIcon("sello4.png");
-		this.sello4B=new JButton(sello4);
+		this.sello4B=new JRadioButton(sello4);
+		this.sello4B.setPreferredSize(new Dimension(80,80));
 		this.add(sello4B);
-		this.sello4B.setPreferredSize(new Dimension(64,64));
 		
 		this.guardar=new ImageIcon("guardar.png");
-		this.guardarB=new JButton(guardar);
+		this.guardarB=new JRadioButton(guardar);
+		this.guardarB.setPreferredSize(new Dimension(80,80));
+		this.guardarB.addActionListener(this);
 		this.add(guardarB);
-		this.guardarB.setPreferredSize(new Dimension(64,64));
 		
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource()==this.guardarB){
+			System.out.println("Guardar");
+		}
 	}
 }
