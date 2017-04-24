2b2e36dd7c0d3e81567e524a02723e61d43702a5
AbigailParrales <danielaparralesd.m@gmail.com>
2017-04-24 17:56:15 -0500

    Temp

---
+++ Cuadrado.java
@@ -0,0 +1,24 @@
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class Cuadrado implements Pintable {
+	private ArrayList<Point> puntos;
+	
+	public Cuadrado(){
+		this.puntos=new ArrayList<>();
+	}
+	
+	public void agregarCoordenada(int x, int y){
+		this.puntos.add(new Point(x,y));
+	}
+	
+	public void pintate(Graphics g) {
+		Point pA=this.puntos.get(0);
+		for(Point p:this.puntos){
+			g.drawRect((int)p.getX(), (int)p.getY(), (int)pA.getX(), (int)pA.getY());
+			pA=p;
+		}
+	}
+
+}
