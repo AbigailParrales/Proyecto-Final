d12a8274ebf40e48f696324345de669c547e3b57
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2017-04-30 22:17:47 -0700

    Condiciones circulo

--- Circulo.java
+++ Circulo.java
@@ -3,22 +3,33 @@ import java.awt.Point;
 import java.util.ArrayList;
 
 public class Circulo extends Pintable{
-private ArrayList<Point> puntos;
-	
+	private ArrayList<Point> puntos;
+
 	public Circulo(){
 		this.puntos=new ArrayList<>();
 	}
-	
+
 	public void agregarCoordenada(int x, int y){
 		this.puntos.add(new Point(x,y));
 	}
-	
+
 	public void pintate(Graphics g) {
-		System.out.println("Estoy en pintar círculo");
+		System.out.println("Estoy en pintar cÃ­rculo");
 		Point p1=this.puntos.get(0);
 		Point p2=this.puntos.get(puntos.size()-1);
-		
-		g.drawOval((int)p1.getX(), (int)p1.getY(), Math.abs((int)p2.getX()-(int)p1.getX()), Math.abs((int)p2.getY()-(int)p1.getY()));
-		
+
+		if((int)p1.getX()<(int)p2.getX() && (int)p1.getY()<(int)p2.getY()){
+			g.drawOval((int)p1.getX(), (int)p1.getY(), (int)p2.getX()-(int)p1.getX(), (int)p2.getY()-(int)p1.getY());
+		}
+		else if((int)p1.getX()<(int)p2.getX() && (int)p2.getY()<(int)p1.getY()){
+			g.drawOval((int)p1.getX(), (int)p2.getY(), (int)p2.getX()-(int)p1.getX(), (int)p1.getY()-(int)p2.getY());
+		}
+		else if((int)p2.getX()<(int)p1.getX() && (int)p1.getY()<(int)p2.getY()){
+			g.drawOval((int)p2.getX(), (int)p1.getY(), (int)p1.getX()-(int)p2.getX(), (int)p2.getY()-(int)p1.getY());
+		}
+		else if((int)p2.getX()<(int)p1.getX() && (int)p2.getY()<(int)p1.getY()){
+			g.drawOval((int)p2.getX(), (int)p2.getY(), (int)p1.getX()-(int)p2.getX(), (int)p1.getY()-(int)p2.getY());
+		}
 	}
 }
+
