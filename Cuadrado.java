8f8bbe92500b8797206a49caf1bcd28d3c3cd93b
FranciscoMarquez1 <fcomarquezb@hotmail.com>
2017-04-30 22:18:21 -0700

    Condiciones Cuadrado

--- Cuadrado.java
+++ Cuadrado.java
@@ -4,21 +4,33 @@ import java.util.ArrayList;
 
 public class Cuadrado extends Pintable {
 	private ArrayList<Point> puntos;
-	
+
 	public Cuadrado(){
 		this.puntos=new ArrayList<>();
 	}
-	
+
 	public void agregarCoordenada(int x, int y){
 		this.puntos.add(new Point(x,y));
 	}
-	
+
 	public void pintate(Graphics g) {
-		System.out.println("Entré a pintar cuadrado");
+		System.out.println("EntrÃ© a pintar cuadrado");
 		Point p1=this.puntos.get(0);
 		Point p2=this.puntos.get(puntos.size()-1);
-		
-		g.drawRect((int)p1.getX(), (int)p1.getY(), (int)p2.getX()-(int)p1.getX(), (int)p2.getY()-(int)p1.getY());
+
+		if((int)p1.getX()<(int)p2.getX() && (int)p1.getY()<(int)p2.getY()){
+			g.drawRect((int)p1.getX(), (int)p1.getY(), (int)p2.getX()-(int)p1.getX(), (int)p2.getY()-(int)p1.getY());
+		}
+		else if((int)p1.getX()<(int)p2.getX() && (int)p2.getY()<(int)p1.getY()){
+			g.drawRect((int)p1.getX(), (int)p2.getY(), (int)p2.getX()-(int)p1.getX(), (int)p1.getY()-(int)p2.getY());
+		}
+		else if((int)p2.getX()<(int)p1.getX() && (int)p1.getY()<(int)p2.getY()){
+			g.drawRect((int)p2.getX(), (int)p1.getY(), (int)p1.getX()-(int)p2.getX(), (int)p2.getY()-(int)p1.getY());
+		}
+		else if((int)p2.getX()<(int)p1.getX() && (int)p2.getY()<(int)p1.getY()){
+			g.drawRect((int)p2.getX(), (int)p2.getY(), (int)p1.getX()-(int)p2.getX(), (int)p1.getY()-(int)p2.getY());
+		}
+
 		/*for(Point p:this.puntos){
 			g.drawRect((int)p.getX(), (int)p.getY(), (int)pA.getX()-(int)p.getX(), (int)pA.getY()-(int)p.getY());
 		}*/
